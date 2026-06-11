4d970af3103456959f1173407ac6ca475c58a805
Sneha Das <snehadas110418@gmail.com>
2026-06-11 23:35:14 +0530

    initial commit

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -4,6 +4,6 @@ const GenerateToken = (id: string, role: string): string => {
     return jwt.sign({ id, role }, process.env.JWT_SECRET as string, {
         expiresIn: '30d'
     });
-};
+}; 
 
 module.exports = { GenerateToken };
